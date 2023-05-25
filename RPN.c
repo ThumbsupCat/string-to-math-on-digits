d881b0ba4d12acd8d97e4a1f77f1085cc006be65
ThumbsupCat <95048853+ThumbsupCat@users.noreply.github.com>
2023-05-25 03:56:20 +0300

    Update RPN.c
    Editting pointer position, coding style

--- RPN.c
+++ RPN.c
@@ -37,7 +37,7 @@ char *reverse(char* string) {
     return string;
 }
 
-char* string_convertor(Stack* rpn_stack) {
+char *string_convertor(Stack* rpn_stack) {
     char *converted = calloc(rpn_stack->size + 1, sizeof(char));
     int k = 0;
 
@@ -72,8 +72,8 @@ int getPrecedence(char ch) {
 char *RPN(char expression[]) {
     int i;
 
-    Stack* rpn = createStack();
-    Stack* operators = createStack();
+    Stack *rpn = createStack();
+    Stack *operators = createStack();
 
     for (i = 0; expression[i]; i++) {
         if (is_digit(expression[i])) {
@@ -115,7 +115,7 @@ int eval(const char* expression) {
     int operand1, operand2;
     int result;
 
-    Stack* stack = createStack();
+    Stack *stack = createStack();
 
     for (i = 0; i < length; i++) {
         char ch = expression[i];
